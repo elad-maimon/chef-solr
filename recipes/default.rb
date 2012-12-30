b20c5c378875608d2457c8e50db615d468f4023b
Almog Kurtser <almog.kurtser@klarna.com>
2012-12-30 17:12:31 +0200

    removed the jetty ENV configuration completely, that should take place as part of the deployment

--- recipes/default.rb
+++ recipes/default.rb
@@ -81,12 +81,3 @@ template "#{node.jetty.home}/contexts/solr.xml" do
   source "solr.context.erb"
   notifies :restart, resources(:service => "jetty")
 end
-
-bash "Add solr to jetty's config" do
-  user "root"
-  cwd "/etc/default"
-  code <<-EOT
-    echo 'JAVA_OPTIONS="$JAVA_OPTIONS -Dsolr.solr.home=#{node.solr.home}"' >> jetty
-  EOT
-  notifies :restart, resources(:service => "jetty")
-end
