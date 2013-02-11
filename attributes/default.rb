e3f5ba3eeac432cf8d5cbc53e8067c4fb5bf448d
Almog Kurtser <almog.kurtser@klarna.com>
2013-02-11 12:05:06 +0200

    solr 3.6.1 -> 3.6.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ include_attribute "jetty"
 
 expand!
 
-default[:solr][:version]   = "3.6.1"
+default[:solr][:version]   = "3.6.2"
 default[:solr][:link]      = "http://www.mirrorservice.org/sites/ftp.apache.org/lucene/solr/#{solr.version}/apache-solr-#{solr.version}.tgz"
 default[:solr][:checksum]  = "1b4552ba95c8456d4fbd596e82028eaa0619b6942786e98e1c4c31258543c708" #sha265
 default[:solr][:directory] = "/usr/local/src"
@@ -11,9 +11,9 @@ default[:solr][:extracted] = "#{solr.directory}/apache-solr-#{solr.version}"
 default[:solr][:war]       = "#{solr.extracted}/dist/apache-solr-#{solr.version}.war"
 
 default[:solr][:context_path]  = 'solr'
-default[:solr][:home]          = "/etc/solr"
-set[:solr][:config]            = node.solr.home + "/conf"
-set[:solr][:lib]               = node.solr.home + "/lib"
-default[:solr][:data]          = "#{node.jetty.home}/webapps/#{node.solr.context_path}/data"
+default[:solr][:home]          = "#{node.jetty.home}/webapps/#{node.solr.context_path}"
+set[:solr][:config]            = "#{node.solr.home}/conf"
+set[:solr][:lib]               = "#{node.solr.home}/lib"
+default[:solr][:data]          = "#{node.solr.home}/data"
 default[:solr][:custom_config] = nil
 default[:solr][:custom_lib]    = nil
--- recipes/default.rb
+++ recipes/default.rb
@@ -20,6 +20,7 @@
 include_recipe "jetty"
 
 remote_file node.solr.download do
+  action :create_if_missing
   source   node.solr.link
   checksum node.solr.checksum
   mode     0644
@@ -43,79 +44,40 @@ directory node.solr.data do
   mode      "750"
 end
 
+directory node.solr.config do
+  owner node.jetty.user
+  group node.jetty.group
+  mode "750"
+  notifies :restart, resources(:service => "jetty")
+end
+
+directory node.solr.lib do
+  owner     node.jetty.user
+  group     node.jetty.group
+  recursive true
+  mode      "750"
+end
+
+remote_file "#{node.solr.lib}/postgresql-9.2-1002.jdbc4.jar" do
+  action :create_if_missing
+  source "http://jdbc.postgresql.org/download/postgresql-9.2-1002.jdbc4.jar"
+  backup 0
+  mode "640"
+  owner node.jetty.user
+  group node.jetty.group
+end
+
+remote_file "#{node.solr.lib}/solr-dataimporthandler-#{node.solr.version}.jar" do
+  action :create_if_missing
+  source "http://repo1.maven.org/maven2/org/apache/solr/solr-dataimporthandler/#{node.solr.version}/solr-dataimporthandler-#{node.solr.version}.jar"
+  backup 0
+  mode "640"
+  owner node.jetty.user
+  group node.jetty.group
+end
+
 template "#{node.jetty.home}/contexts/solr.xml" do
   owner  node.jetty.user
   source "solr.context.erb"
   notifies :restart, resources(:service => "jetty")
 end
-
-remote_directory node.solr.config do
-  source       "sunspot-1.2.1"
-  owner        node.jetty.user
-  group        node.jetty.group
-  files_owner  node.jetty.user
-  files_group  node.jetty.group
-  files_backup 0
-  files_mode   "644"
-  purge        true
-
-  notifies     :restart, resources(:service => "jetty"), :immediately
-  not_if       { File.exists? node.solr.config }
-end
-
-
-if node.solr.custom_lib
-
-  bash "Copy custom lib to solr" do
-    code <<-EOH
-      rm -rf #{node.solr.lib}
-      cp -r #{node.solr.custom_lib} #{node.solr.lib}
-      chown -R #{node.jetty.user}:#{node.jetty.group} #{node.solr.lib}
-      find #{node.solr.lib} -type f -exec chmod 640 \\;
-      find #{node.solr.lib} -type d -exec chmod 750 \\;
-    EOH
-    notifies     :restart, resources(:service => "jetty"), :immediately
-    # Only copy the lib if it exists, and it is different from what is already there
-    only_if <<-EOH
-      test -e #{node.solr.custom_lib} &&
-      ( diff -r #{node.solr.custom_lib} #{node.solr.lib}; test $? != 0 )
-    EOH
-  end
-
-end
-
-if node.solr.custom_config
-
-  bash "Copy custom config to solr" do
-    code <<-EOH
-      rm -rf #{node.solr.config}
-      cp -r #{node.solr.custom_config} #{node.solr.config}
-      chown -R #{node.jetty.user}:#{node.jetty.group} #{node.solr.config}
-      find #{node.solr.config} -type f -exec chmod 640 \\;
-      find #{node.solr.config} -type d -exec chmod 750 \\;
-    EOH
-    notifies     :restart, resources(:service => "jetty"), :immediately
-    # Only copy the config if it exists, and it is different from what is already there
-    only_if <<-EOH
-      test -e #{node.solr.custom_config}/solrconfig.xml &&
-      ( diff -r #{node.solr.custom_config} #{node.solr.config}; test $? != 0 )
-    EOH
-  end
-
-end
-
-remote_directory "/etc/solr/conf" do
-  source       "sunspot-1.2.1"
-  owner        node.jetty.user
-  group        node.jetty.group
-  files_owner  node.jetty.user
-  files_group  node.jetty.group
-  files_backup 0
-  files_mode   "644"
-  purge        true
-  notifies     :restart, resources(:service => "jetty")
-  not_if       "test -e #{node.solr.custom_config}/solrconfig.xml"
-end
-
-
-
