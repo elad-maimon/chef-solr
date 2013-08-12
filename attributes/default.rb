a6c7fee17a93e20e9e7c36c108536845e85eedae
Almog Kurtser <almog.kurtser@klarna.com>
2013-08-12 16:21:00 +0300

    Use archive url instead of latest stable

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ include_attribute "jetty"
 expand!
 
 default[:solr][:version]   = "4.4.0"
-default[:solr][:link]      = "http://www.mirrorservice.org/sites/ftp.apache.org/lucene/solr/#{solr.version}/solr-#{solr.version}.tgz"
+default[:solr][:link]      = "https://archive.apache.org/dist/lucene/solr/#{solr.version}/solr-#{solr.version}.tgz"
 default[:solr][:checksum]  = "99c27527122fdc0d6eba83ced9598bf5cd3584954188b32cb2f655f1e810886b" #sha265
 default[:solr][:directory] = "/usr/local/src"
 default[:solr][:download]  = "#{solr.directory}/solr-#{solr.version}.tgz"
